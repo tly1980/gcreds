5e3b87d884792c45f92e9b04053fcd680b573eb0
Tang, Tom <Tom.Tang@news.com.au>
2017-10-11 10:54:12 +1100

    better readme

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
 DEV_ENV := py27
+PY_IMG:=$(shell python2 -c "import urllib, setup; print urllib.quote('python-{}-blue.svg'.format(' '.join(setup.PY_VERSIONS)))")
+
+README.md: _README.md
+	echo "![supported python versions]($(PY_IMG))" > README.md
+	cat _README.md >> README.md
 
 README.rst: README.md
 	pandoc --from=markdown --to=rst --output=README.rst README.md
 
 package: README.rst
+
 	python setup.py bdist_wheel --universal
 
 test:
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ name = 'gcreds'
 with open("{name}.py".format(name=name), encoding='utf-8') as f:
   version = re.search("^__version__\s*=\s*[\'\"]([^\'\"]+)", f.read(), flags=re.I | re.M).group(1)
 
-with open('README.rst', encoding='utf-8') as f:
+with open('README.rst', encoding='utf8') as f:
   long_description = f.read()
 
 install_requires = [
@@ -21,12 +21,33 @@ install_requires = [
     'google-auth-httplib2'
 ]
 
-with open('req.txt', 'wb') as f:
-  for i in install_requires:
-    f.write((i + '\n').encode('utf8'))
+PY_VERSIONS = [
+    '2.7',
+    '3.4',
+    '3.5',
+    '3.6',
+]
+
+# https://pypi.python.org/pypi?:action=list_classifiers
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Plugins',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+]
+
+classifiers += [
+    'Programming Language :: Python :: {v}'.format(v=v) for v in PY_VERSIONS]
 
 
-setup(
+## generate the req.txt
+if __name__ == '__main__':
+  with open('req.txt', 'wb') as f:
+    for i in install_requires:
+      f.write((i + '\n').encode('utf8'))
+
+  setup(
     name=name,
     version=version,
     description='gcreds - a credential management tool using google cloud',
@@ -38,20 +59,5 @@ setup(
     scripts=['bin/gcreds'],
     install_requires=install_requires,
     license="MIT",
-    classifiers=[ # https://pypi.python.org/pypi?:action=list_classifiers
-        'Development Status :: 4 - Beta',
-        'Environment :: Plugins',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-)
+    classifiers=classifiers,
+  )
